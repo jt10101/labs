34955e54358cfd355ba6b7a8147c59837fd77420
Jacob Tham <jacobtham3@gmail.com>
2025-05-01 15:43:37 +0800

    leveup ex 20

--- intro-to-javascript-objects-lab/app.js
+++ intro-to-javascript-objects-lab/app.js
@@ -159,11 +159,11 @@ Also, log the `game.items` array to confirm that the pokeball quantity is being 
 Solve Exercise 11 here:
 */
 
-game.catchPokemon = function (pokemonObj) {
-  game.party.push(pokemonObj);
-  game.items[1].quantity = game.items[1].quantity - 1;
-};
-game.catchPokemon(pokemon[12]);
+// game.catchPokemon = function (pokemonObj) {
+//   game.party.push(pokemonObj);
+//   game.items[1].quantity = game.items[1].quantity - 1;
+// };
+// game.catchPokemon(pokemon[12]);
 // console.log(game.items[1].quantity);
 
 /*
@@ -263,4 +263,154 @@ Exercise 16
 Solve Exercise 16 here:
 */
 
-console.log(game);
+// console.log(game);
+
+/*
+Exercise 17
+1. Arrange the Pokémon in `game.party` by their HP. The one with the highest HP should come first.
+2. You'll need to use the `.sort()` method. How does the compare function work in sorting numbers?
+
+
+Solve Exercise 17 here:
+*/
+
+// console.log(game.party.sort((a, b) => a.hp - b.hp)); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+/*
+Exercise 18
+Add a new property to the `game` object called `collection` and initialize its value to an empty array.
+
+Copy the `catchPokemon` method you wrote in Exercise Twelve and paste it below. Modify it so that:
+  - Ensure that no more than six Pokemon can be in the party at any time. 
+    Excess Pokemon should be placed in the `game.collection` array.
+  - It's up to you how to distribute Pokemon in a situation where more than six 
+    would be placed into the `game.party` array.
+
+Again, for this exercise, it's okay to have a negative number of pokeballs.
+
+After updating the method, use it by calling it and passing in a pokemon object of your choice from the `pokemon` data to catch it.
+
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 18 here:
+*/
+
+game.collection = [];
+// game.catchPokemon = function (pokemonObj) {
+//   if (Object.keys(game.party).length < 6) {
+//     game.party.push(pokemonObj);
+//     game.items[1].quantity = game.items[1].quantity - 1;
+//   } else game.collection.push(pokemonObj);
+//   game.items[1].quantity = game.items[1].quantity - 1;
+// };
+// game.catchPokemon(pokemon[13]);
+// game.catchPokemon(pokemon[14]);
+// game.catchPokemon(pokemon[17]);
+
+// console.log(`You have ${game.items[1].quantity} Pokeballs remaining.`);
+// console.log(
+//   `You have ${Object.keys(game.party).length} Pokemon in your party.`
+// );
+// console.log(
+//   `You have ${Object.keys(game.collection).length} Pokemon in your collection.`
+// );
+// console.log(game.party);
+// console.log(game.collection);
+
+/*
+Exercise 19
+Copy the `catchPokemon` method that you just wrote above, and paste it below. The time has come to make it so that we cannot catch a Pokemon when we do not have any pokeballs to catch it with. 
+
+Modify the method so that if there are no pokeballs a message will be displayed that there are not enough pokeballs to catch the desired Pokemon.
+
+Also, ensure that the Pokemon isn't added to the `game.party` or the `game.collection`.
+
+Solve Exercise 19 here:
+*/
+
+// game.catchPokemon = function (pokemonObj) {
+//   if (game.items[1].quantity === 0) {
+//     console.log(
+//       `${game.items[1].quantity} Pokeballs left, cannot catch Pokemon!`
+//     );
+//   } else {
+//     if (Object.keys(game.party).length < 6) {
+//       game.party.push(pokemonObj);
+//       game.items[1].quantity = game.items[1].quantity - 1;
+//     } else game.collection.push(pokemonObj);
+//     game.items[1].quantity = game.items[1].quantity - 1;
+//   }
+// };
+
+// game.catchPokemon(pokemon[13]);
+// game.catchPokemon(pokemon[14]);
+// game.catchPokemon(pokemon[102]);
+// game.catchPokemon(pokemon[113]);
+// game.catchPokemon(pokemon[83]);
+// game.catchPokemon(pokemon[5]);
+// game.catchPokemon(pokemon[72]);
+
+// console.log(`You have ${game.items[1].quantity} Pokeballs remaining.`);
+// console.log(
+//   `You have ${Object.keys(game.party).length} Pokemon in your party.`
+// );
+// console.log(
+//   `You have ${Object.keys(game.collection).length} Pokemon in your collection.`
+// );
+
+/*
+Exercise 20
+Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify is so that you can just pass in the name of a Pokemon instead of an entire object, and the method will look up the Pokemon from the data set for you.
+
+The string passed in should be allowed to be any case (for example, if the string 'PiKacHU' is passed to the function, it should match to 'Pikachu' in the data set). 
+
+If there is not a match, then return a string noting that the selected Pokemon does not exist. Ensure you do not decrement the pokeball count if an invalid Pokemon name is passed in, and also ensure that the Pokemon isn't added to the `game.party` or the `game.collection`.
+
+Solve Exercise 20 here:
+*/
+
+const pokemonNameArray = pokemon.map((a) => a.name); // https://stackoverflow.com/questions/19590865/from-an-array-of-objects-extract-value-of-a-property-as-array
+const pokemonNameArrayLower = pokemonNameArray.map((element) =>
+  element.toLowerCase()
+);
+let y = 0;
+game.catchPokemon = function (pokemonName) {
+  let x = pokemonName.toLowerCase();
+  if (pokemonNameArrayLower.includes(x) === false) {
+    console.log(`Pokemon ${pokemonName} does not exist!`);
+  } // This segment runs if Pokemon name does not exist in data.js
+  else {
+    let y = pokemonNameArrayLower.indexOf(x); // identifies the pokemon's object index from data.js file
+    let z = pokemon[y].name; // Obtains the correct Pokemon Name from data.js
+    if (game.items[1].quantity === 0) {
+      // Start of code when Pokemon exists
+      console.log(
+        `${game.items[1].quantity} Pokeballs left, cannot catch ${z}!`
+      ); // First checks if have sufficient Pokeballs to catch Pokemon
+    } else {
+      if (Object.keys(game.party).length < 6) {
+        game.party.push(pokemon[y]);
+        game.items[1].quantity = game.items[1].quantity - 1;
+        console.log(
+          `Captured ${z}! | Party: ${game.party.length} | Collection: ${game.collection.length} | Pokeballs: ${game.items[1].quantity}`
+        );
+      } else {
+        game.collection.push(pokemon[y]);
+        game.items[1].quantity = game.items[1].quantity - 1;
+        console.log(
+          `Party full! ${z} sent to collection!| Party: ${game.party.length} | Collection: ${game.collection.length} | Pokeballs: ${game.items[1].quantity}`
+        );
+      }
+    }
+  }
+};
+
+game.catchPokemon("Pikachu");
+game.catchPokemon("zubaT");
+game.catchPokemon("mEw");
+game.catchPokemon("mewTwO");
+game.catchPokemon("raicHu");
+game.catchPokemon("Zapdos");
+game.catchPokemon("Articuno");
+game.catchPokemon("Moltres");
+game.catchPokemon("Dragonite");
